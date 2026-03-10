04a5385e41db9efeeadb95b9249524c327e0d8c7
Guilherme Uzeda <gui.c.uzeda@gmail.com>
2026-03-10 18:38:00 -0300

    fix: remove missing sonarcfamily analyzer to fix startup crash
    - Also added lazy load events so it triggers reliably

--- lua/custom/plugins/sonarlint.lua
+++ lua/custom/plugins/sonarlint.lua
@@ -4,6 +4,7 @@ return {
     'neovim/nvim-lspconfig',
     'lewis6991/gitsigns.nvim', -- Optional: Required for SCM/Connected Mode features
   },
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
     -- Determine the path to the mason installation directory
     local mason_root = vim.fn.stdpath('data') .. '/mason'
@@ -16,7 +17,6 @@ return {
           '-analyzers',
           -- Add paths to the analyzers you need here. 
           -- Mason typically installs them in share/sonarlint-analyzers/
-          mason_root .. '/share/sonarlint-analyzers/sonarcfamily.jar',
           mason_root .. '/share/sonarlint-analyzers/sonarjava.jar',
           mason_root .. '/share/sonarlint-analyzers/sonarpython.jar',
           mason_root .. '/share/sonarlint-analyzers/sonargo.jar',
