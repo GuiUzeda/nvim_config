4144ece8cd7caf7b095dafc9b3b71d679c09eaad
Guilherme Uzeda <gui.c.uzeda@gmail.com>
2026-03-11 15:50:33 -0300

    feat(markdown): add keymaps for vim-table-mode
    - Map '<leader>tr' to TableModeRealign to easily format messy markdown tables
    - Map '<leader>tm' to TableModeToggle

--- lua/custom/plugins/table-mode.lua
+++ lua/custom/plugins/table-mode.lua
@@ -1,6 +1,10 @@
 return {
   'dhruvasagar/vim-table-mode',
   ft = 'markdown',
+  keys = {
+    { '<leader>tm', '<cmd>TableModeToggle<cr>', desc = '[T]able [M]ode Toggle' },
+    { '<leader>tr', '<cmd>TableModeRealign<cr>', desc = '[T]able [R]ealign' },
+  },
   config = function()
     -- Enable Markdown-compatible table corner characters
     vim.g.table_mode_corner = '|'
