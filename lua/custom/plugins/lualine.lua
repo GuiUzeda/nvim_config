578f1bdb037d6ef32a63c8875b2a747b4982775c
Guilherme <gui.c.uzeda@gmail.com>
2026-03-21 10:17:23 -0300

    style: change lualine separators to slash style to match tmux

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -30,12 +30,12 @@ return {
 
     require('lualine').setup {
       options = {
-        theme = 'tokyonight', -- or bubble_theme if you prefer
-        component_separators = '|',
-        section_separators = { left = '', right = '' },
+        theme = 'tokyonight',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
       },
       sections = {
-        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
         lualine_b = { 'filename', 'branch' },
         lualine_c = {
           '%=', -- center align
@@ -54,7 +54,7 @@ return {
         },
         lualine_y = { 'filetype', 'progress' },
         lualine_z = {
-          { 'location', separator = { right = '' }, left_padding = 2 },
+          { 'location', separator = { right = '' }, left_padding = 2 },
         },
       },
       inactive_sections = {
