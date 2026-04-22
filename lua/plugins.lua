eb5ace45c58b247c061216f2e8087a44a5f6701f
Guilherme <gui.c.uzeda@gmail.com>
2026-04-22 07:48:14 -0300

    fix(ui): Remove noice.nvim to prevent conflict with native ui2

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,64 +1,73 @@
 -- lua/plugins.lua
 -- Native plugin management via Neovim 0.12 vim.pack
 
--- Shorthands: gh: for GitHub, cb: for Codeberg
 local plugins = {
   -- Core Utils
-  'gh:NMAC427/guess-indent.nvim',
-  'gh:lewis6991/gitsigns.nvim',
-  'gh:folke/which-key.nvim',
+  'https://github.com/NMAC427/guess-indent.nvim',
+  'https://github.com/lewis6991/gitsigns.nvim',
+  'https://github.com/folke/which-key.nvim',
   
   -- Telescope
-  'gh:nvim-lua/plenary.nvim',
-  'gh:nvim-telescope/telescope.nvim',
-  'gh:nvim-telescope/telescope-fzf-native.nvim',
-  'gh:nvim-telescope/telescope-ui-select.nvim',
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/nvim-telescope/telescope.nvim',
+  'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
+  'https://github.com/nvim-telescope/telescope-ui-select.nvim',
   
   -- Completion & LSP
-  'gh:saghen/blink.cmp',
-  'gh:L3MON4D3/LuaSnip',
-  'gh:rafamadriz/friendly-snippets',
-  'gh:folke/lazydev.nvim',
-  'gh:neovim/nvim-lspconfig',
-  'gh:mason-org/mason.nvim',
-  'gh:mason-org/mason-lspconfig.nvim',
-  'gh:WhoIsSethDaniel/mason-tool-installer.nvim',
+  'https://github.com/saghen/blink.cmp',
+  'https://github.com/L3MON4D3/LuaSnip',
+  'https://github.com/rafamadriz/friendly-snippets',
+  'https://github.com/folke/lazydev.nvim',
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/mason-org/mason.nvim',
+  'https://github.com/mason-org/mason-lspconfig.nvim',
+  'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim',
   
   -- UI & Aesthetics
-  'gh:uloco/bluloco.nvim',
-  'gh:rktjmp/lush.nvim',
-  'gh:folke/tokyonight.nvim',
-  'gh:folke/todo-comments.nvim',
-  'gh:echasnovski/mini.nvim',
-  'gh:nvim-tree/nvim-web-devicons',
-  'gh:nvim-lualine/lualine.nvim',
-  'gh:rcarriga/nvim-notify',
-  'gh:stevearc/dressing.nvim',
-  'gh:folke/noice.nvim',
-  'gh:goolord/alpha-nvim',
+  'https://github.com/uloco/bluloco.nvim',
+  'https://github.com/rktjmp/lush.nvim',
+  'https://github.com/folke/tokyonight.nvim',
+  'https://github.com/folke/todo-comments.nvim',
+  'https://github.com/echasnovski/mini.nvim',
+  'https://github.com/nvim-tree/nvim-web-devicons',
+  'https://github.com/nvim-lualine/lualine.nvim',
+  'https://github.com/rcarriga/nvim-notify',
+  'https://github.com/stevearc/dressing.nvim',
+  'https://github.com/goolord/alpha-nvim',
   
   -- PKM
-  'gh:epwalsh/obsidian.nvim',
+  'https://github.com/epwalsh/obsidian.nvim',
   
   -- Navigation & Multiplexer
-  'gh:mrjones2014/smart-splits.nvim',
+  'https://github.com/mrjones2014/smart-splits.nvim',
   
   -- Editing
-  'gh:stevearc/conform.nvim',
-  'gh:mfussenegger/nvim-lint',
-  'gh:windwp/nvim-autopairs',
-  'gh:nvim-neo-tree/neo-tree.nvim',
-  'gh:antosha417/nvim-lsp-file-operations',
-  'gh:MunifTanjim/nui.nvim',
+  'https://github.com/stevearc/conform.nvim',
+  'https://github.com/mfussenegger/nvim-lint',
+  'https://github.com/windwp/nvim-autopairs',
+  'https://github.com/nvim-neo-tree/neo-tree.nvim',
+  'https://github.com/antosha417/nvim-lsp-file-operations',
+  'https://github.com/MunifTanjim/nui.nvim',
 }
 
 -- Add all plugins
 vim.pack.add(plugins)
 
--- Automatically load all configuration files in lua/config/
-local config_path = vim.fn.stdpath('config') .. '/lua/config'
-for _, file in ipairs(vim.fn.readdir(config_path)) do
-  if file:match('%.lua$') then
-    require('config.' .. file:gsub('%.lua$', ''))
+-- Helper to safely load configuration
+local function safe_require(module)
+  local ok, err = pcall(require, 'config.' .. module)
+  if not ok then
+    vim.notify('Error loading config for ' .. module .. ': ' .. err, vim.log.levels.ERROR)
   end
+end
+
+-- Load configurations in a logical order
+local configs = {
+  'ui', 'bluloco', 'alpha', 'lualine', 'which-key', 'telescope',
+  'blink', 'conform', 'gitsigns', 'todo-comments', 'mini',
+  'neo-tree', 'autopairs', 'obsidian', 'smart-splits'
+}
+
+for _, name in ipairs(configs) do
+  safe_require(name)
 end
