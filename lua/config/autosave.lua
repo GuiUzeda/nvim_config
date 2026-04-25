b2e6c6fd509d39c0c5db8146f7ba5d4006b1dce2
Guilherme <gui.c.uzeda@gmail.com>
2026-04-25 11:50:25 -0300

    fix(editing): Remove deprecated execution_message from auto-save

--- lua/config/autosave.lua
+++ lua/config/autosave.lua
@@ -1,11 +1,6 @@
 -- lua/config/autosave.lua
 require('auto-save').setup {
   enabled = true,
-  execution_message = {
-    message = function() return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S")) end,
-    dim = 0.18,
-    cleaning_interval = 1250,
-  },
   trigger_events = { "InsertLeave", "TextChanged" },
   condition = function(buf)
     local fn = vim.fn
